1c224accf37910504f75670f551928f8858d0e54
Quasar Chunawala <quasar.chunawala@gmail.com>
2019-11-24 08:39:33 +0100

    hello_world.cpp - This program prints messages on the display terminal

---
+++ modern_cpp_programming/intro_to_c++/hello_world.cpp
@@ -0,0 +1,11 @@
+// hello_world.cpp - This program prints messages on the display terminal
+
+#include <iostream>                     //a preprocessor directive
+
+int main()                              //Function header
+{                                       //Curly braces express grouping
+    std::cout << "Hello World";         //message
+    std::cout << std::endl;             //start a new line
+    std::cout << "C++ is great fun!";
+    return 0;                           //return 0 to the caller
+}                                       //end of main function
